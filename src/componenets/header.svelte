37833ae27a67842433a510f871158863bed648f2
Wesley McGhee <wesmcghee6372@gmail.com>
2023-11-27 20:12:22 -0600

    BATMAN

---
+++ src/componenets/header.svelte
@@ -0,0 +1,25 @@
+<header>
+  <h1 class="logo">WM</h1>
+  <div>
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a class="nav-link" href="/">home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">experince</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/">connect with me</a>
+      </li>
+    </ul>
+  </div>
+</header>
+
+<style>
+  .logo {
+    font-size: 2.5rem;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "../componenets/header.svelte";
+</script>
+
+<Header />
+
+<h1>Welcome to my Site</h1>
+
+<style>
+  * {
+    font-family: "Roboto", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
